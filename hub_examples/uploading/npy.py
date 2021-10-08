ed84577e103361bf2ba14a77718e3ba281e70767
McCrearyD <mccreary@dyllan.ai>
2021-10-08 15:25:21 -0700

    comments

--- hub_examples/uploading/npy.py
+++ hub_examples/uploading/npy.py
@@ -12,12 +12,12 @@ def upload(uri: str):
     ds.create_tensor("x")
     ds.create_tensor("images", htype="image", sample_compression="png")
 
-    # add some uncompressed numpy data
+    # add some numpy data and keep uncompressed
     ds.x.append(np.ones((10, 10)))
     
     # add some numpy data and compress as PNG
     data = np.random.randint(low=0, high=256, size=(100, 100, 100, 3), dtype="uint8")
-    ds.images.extend(data)
+    ds.images.extend(data)  # 100 random images, each 100x100x3
 
 
 def visualize(uri: str):
@@ -25,11 +25,12 @@ def visualize(uri: str):
 
     ds = hub.load(uri, read_only=True)
 
-    Image.fromarray(ds.images[0].numpy()).show()
     Image.fromarray(ds.x[0].numpy()).show()
+    Image.fromarray(ds.images[0].numpy()).show()
 
 
 if __name__ == "__main__":
     uri = "./_datasets/npy"
+    
     upload(uri)
     visualize(uri)
